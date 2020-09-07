603e3573b33a80499f9094db9e5e23a913dd0e42
Leo <personne@quedalle.rien>
2020-09-07 18:44:25 +0200

    Controleur Jour

---
+++ controleur/Jour.php
@@ -0,0 +1,44 @@
+<?php
+  /**
+   *
+   */
+  class Jour
+  {
+    private $time;
+    private $evenement;
+    function __construct($time)
+    {
+      $this->time =  $time;
+      $this->loadEvent();
+    }
+
+    function vue(){
+
+    }
+
+    function getNom(){
+      return date('l d F', $this->getTime());
+    }
+
+    function getTime(){
+      return $this->time;
+    }
+
+    function getEvenements(){
+      return $this->evenement;
+    }
+
+    private function loadEvent(){
+      $json = file_get_contents('http://api.pacary.net/AgendaInsaRouen/index.php?fo=2020-ING-ASI-S7&ty=day&ts=20200907');
+      $obj = json_decode($json);
+      foreach ($obj as $key => $value) {
+        $this->evenement = $value;
+        breck;
+      }
+    }
+
+
+  }
+
+
+?>
