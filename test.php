d83956bab53ea293c32b7550201c7c1f4a59f0d4
Leo <personne@quedalle.rien>
2020-09-10 01:26:31 +0200

    test

--- test.php
+++ test.php
@@ -6,7 +6,7 @@
 	require('FileIcsGenerator.php');
 	$o = new EventICal();
 	$a = new FileIcsGenerator();
-	$o = $o->setStart('20200910100000')->setEnd('20200910110000')->setSummary('Espagnole')->setDescription('Espagnole');
+	$o = $o->setStart('20200910100000')->setEnd('20200910110000')->setSummary('Francais')->setDescription('Francais');
 	$a->setCalName('LEOPACARYTEST')->addEvent($o);
 
 	$monfichier = fopen('test.ics', 'a+');
